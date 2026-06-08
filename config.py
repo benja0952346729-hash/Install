12d2885bbc18d1ec83aa7539cbd61f27f418e5d3
benja0952346729-hash <benja0952346729@gmail.com>
2026-06-08 17:57:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_IDS = [int(os.environ.get("ADMIN_ID", "0"))]
+ADMIN_IDS = [int(x.strip()) for x in os.environ.get("ADMIN_IDS", "0").split(",") if x.strip()]
 DATABASE_URL = os.environ.get("DATABASE_URL")
 GROUP_ID = int(os.environ.get("GROUP_ID", "0"))
+GROUP_CHAT_ID = os.environ.get("GROUP_CHAT_ID", str(os.environ.get("GROUP_ID", "0")))
