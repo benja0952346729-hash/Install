2e94d54655417a89b5ba2ff1091eb17d245ec142
benja0952346729-hash <benja0952346729@gmail.com>
2026-06-08 14:42:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_IDS = [123456789]
+import os
 
-DATABASE_URL = "postgresql://neondb_owner:PASSWORD@ep-xxx.neon.tech/neondb"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_IDS = [123456789]
+DATABASE_URL = os.environ.get("DATABASE_URL")
