bc0b0d9b440b09344c3d46d8c8f8a464610d8514
benja0952346729-hash <benja0952346729@gmail.com>
2026-06-08 14:41:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-ADMIN_IDS = [123456789]  # Admin Telegram user IDs
+ADMIN_IDS = [123456789]
 
-# PostgreSQL
-DB_HOST = "localhost"
-DB_PORT = 5432
-DB_NAME = "lottery_bot"
-DB_USER = "postgres"
-DB_PASS = "your_password"
+DATABASE_URL = "postgresql://neondb_owner:PASSWORD@ep-xxx.neon.tech/neondb"
