4cbd0dfe3b6a638c3462afed05acf386560c0102
benja0952346729-hash <benja0952346729@gmail.com>
2026-06-14 10:34:44 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -365,7 +365,7 @@ Respond ONLY in JSON:
 
     try:
         response = _call_groq_with_rotation(lambda client: client.chat.completions.create(
-            model="meta-llama/llama-4-maverick-17b-128e-instruct",
+            model="meta-llama/llama-4-scout-17b-16e-instruct",
             messages=[{
                 "role": "user",
                 "content": [
